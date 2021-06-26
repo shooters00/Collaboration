e426f7b61df2b4e52bfc6bc36a0910b31297d7e4
shooters00 <shooters0000@gmail.com>
2021-06-26 13:10:31 -0400

    js and index

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,19 @@
-var searchBtnEl = document.querySelector('#searchBtn');
+var searchBtnEl = document.getElementById("searchBtn");
 var baseApiUrl = "https://www.loc.gov/";
-
-var specApiUrl = "?fo=json";
+var specApiUrl = "&fo=json";
 
 //Take in city and get weather
-function search() {
-
-    var text = document.querySelector("searchText").textContent;
-    //var type = document.querySelector("dropdown-menu").textContent;
+function search(event) {
+    event.preventDefault();
     console.log("Here");
 
-    fetch(baseApiUrl + "maps" + specApiUrl, {
-        //units="imperial"
+    var text = document.querySelector(".searchText").textContent;
+    var type = document.querySelector(".dropdown-menu").textContent;
+    //text = "civil war";
+
+    
+    fetch(baseApiUrl + "maps/" + "?q=" + text + specApiUrl, {
+        
     })
     .then(function (response) {
         return response.json();
