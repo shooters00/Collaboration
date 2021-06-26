723f348ea0d8efebfb494d7f00d9a591f7dac965
shooters00 <shooters0000@gmail.com>
2021-06-26 12:42:26 -0400

    updated script.js with initial api call

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+var searchBtnEl = document.querySelector('#searchBtn');
+var baseApiUrl = "https://www.loc.gov/";
+
+var specApiUrl = "?fo=json";
+
+//Take in city and get weather
+function search() {
+
+    var text = document.querySelector("searchText").textContent;
+    //var type = document.querySelector("dropdown-menu").textContent;
+    console.log("Here");
+
+    fetch(baseApiUrl + "maps" + specApiUrl, {
+        //units="imperial"
+    })
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+      });
+}
+
+searchBtnEl.addEventListener("click", search);
